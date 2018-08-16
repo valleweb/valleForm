1b79e68f7b94c34fe05060171bc5f2fbaab51ff9
afonsopacifer <afonsopolis@gmail.com>
2018-08-16 00:38:49 -0300

    Add Equipamentos.json mock and fix text font

--- stories/Main.js
+++ stories/Main.js
@@ -3,12 +3,17 @@ import ValleForm from '../src/Main'; // This is our component
 import { setAddon, storiesOf } from '@storybook/react';
 import JSXAddon from 'storybook-addon-jsx';
 import pacientes from '../data/pacientes.json';
+import equipamentos from '../data/equipamentos.json';
 
 setAddon(JSXAddon);
 
 const rows = pacientes.tabs[0].lines;
 const baseApi = 'http://localhost:3000/data';
 const canonicalApi = pacientes.api;
+
+const rows2 = equipamentos.tabs[0].lines;
+const baseApi2 = 'http://localhost:3000/data';
+const canonicalApi2 = equipamentos.api;
 
 storiesOf('ValleForm', module)
   .addWithJSX('Pacientes', () => {
@@ -19,4 +24,13 @@ storiesOf('ValleForm', module)
         canonicalApi = { canonicalApi } 
         params = { {id: ''} } />
     )
-  });
+  })
+  .addWithJSX('Equipamentos', () => {
+    return (
+       <ValleForm 
+        rows = { rows2 } 
+        baseApi = { baseApi2 } 
+        canonicalApi = { canonicalApi2 } 
+        params = { {id: ''} } />
+    )
+  })

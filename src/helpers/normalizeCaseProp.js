48b6eb1edbcb5318eeed7cf4a0cc2157ae72f151
afonsopacifer <afonsopolis@gmail.com>
2018-09-08 03:30:42 -0300

    Add all variations of valle-input tests (insert mode) # 9

--- src/helpers/normalizeCaseProp.js
+++ src/helpers/normalizeCaseProp.js
@@ -1,15 +1,12 @@
-const isValidBooleanProp = prop => (prop.trim());
 const isCapitalizeProp = prop => (prop === 'ProperCase');
 
 const normalizeCaseProp = prop => {
 
 	const booleanProp = {};
 
-	if (isValidBooleanProp(prop)) {
-		isCapitalizeProp(prop)
-			? (booleanProp['capitalize'] = true) // Set Capitalize (normalized )prop
-			: (booleanProp[prop.toLowerCase()] = true); // Set any other boolean (case) prop
-	}
+	isCapitalizeProp(prop)
+		? (booleanProp['capitalize'] = true) // Set Capitalize (normalized )prop
+		: (booleanProp[prop] = true); // Set any other boolean (case) prop
 
 	return booleanProp;
 

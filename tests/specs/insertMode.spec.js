6433d1998b7646ffb734b07e72428934e8f984d4
afonsopacifer <afonsopolis@gmail.com>
2018-09-08 00:40:53 -0300

    Add all basic valle-input (insert-mode) tests #9

--- tests/specs/insertMode.spec.js
+++ tests/specs/insertMode.spec.js
@@ -15,23 +15,102 @@ describe('Insert mode', () => {
     // setup
     // =============== 
 
-    let valleInputSearch = null;
+    let simpleValleInput = null;
 
     beforeEach(() => {
-      const wrapper = shallow(<ValleForm rows = { SingleValleInput.lines } />);
-      valleInputSearch = wrapper.find('valle-input');
+      const ValleFormWrapper = shallow(<ValleForm rows = { SingleValleInput.lines } />);
+      simpleValleInput = ValleFormWrapper.find('valle-input');
     });
 
     // ===============
 
     it('Should render a single valle-input', () => {
-      expect(valleInputSearch).to.have.length(1);
+      expect(simpleValleInput).to.have.length(1);
+      expect(simpleValleInput.type()).to.equal('valle-input');
     });
 
     // ===============
 
     it('Should render a valle-input with correct class', () => {
-      expect(valleInputSearch.props().class).to.equal('valleForm__input');
+      expect(simpleValleInput.props().class).to.equal('valleForm__input');
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct type', () => {
+      expect(simpleValleInput.props().type).to.equal('text');
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct label', () => {
+      expect(simpleValleInput.props().label).to.equal('First field');
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct placeholder', () => {
+      expect(simpleValleInput.props().placeholder).to.equal('Text here');
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct helpertext', () => {
+      expect(simpleValleInput.props().helpertext).to.equal('Full name');
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct errortext', () => {
+      expect(simpleValleInput.props().errortext).to.equal('Required field');
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct data-valle-field', () => {
+      expect(simpleValleInput.props()['data-valle-field']).to.equal('firstfield');
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct maxlength', () => {
+      expect(simpleValleInput.props().maxlength).to.equal(7);
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct id', () => {
+      expect(simpleValleInput.props().id).to.equal('firstfield');
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct pattern', () => {
+      expect(simpleValleInput.props().pattern).to.equal(null);
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct required', () => {
+      expect(simpleValleInput.props().required).to.be.true;
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct readonly', () => {
+      expect(simpleValleInput.props().readonly).to.be.undefined;
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct case', () => {
+      expect(simpleValleInput.props().capitalize).to.be.true;
+    });
+
+    // ===============
+
+    it('Should render a valle-input with correct mask', () => {
+      expect(simpleValleInput.props().mask).to.be.undefined;
     });
 
     // ===============

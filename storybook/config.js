78735319f153231123a0f75d2335fd07d0936977
afonsopacifer <afonsopacifer@live.com>
2020-03-07 01:57:25 -0300

    Add speed dial webcomponents in storybook

--- storybook/config.js
+++ storybook/config.js
@@ -5,6 +5,9 @@ import '../bower_components/polymer/polymer.html';
 import '../bower_components/valle-input/valle-input.html';
 import '../bower_components/valle-select/valle-select.html';
 import '../bower_components/valle-select/valle-option.html';
+import '../bower_components/valle-fab/valle-fab.html';
+import '../bower_components/valle-speed-dial/valle-speed-dial.html';
+import '../bower_components/valle-speed-dial/valle-speed-dial-action.html';
 
 function loadStories() {
   require('../stories/ValleForm.js');

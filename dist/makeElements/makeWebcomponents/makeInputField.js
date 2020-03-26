b296e908a7379bd8a2046d136d76fa5b695ec512
afonsopacifer <afonsopacifer@live.com>
2020-03-26 04:00:00 -0300

    Build 1.1.0

--- dist/makeElements/makeWebcomponents/makeInputField.js
+++ dist/makeElements/makeWebcomponents/makeInputField.js
@@ -42,15 +42,28 @@ exports['default'] = function (field) {
 
 	;var customDescriptionStyle = field.label == 'Descrição' ? 'valleForm__input--description' : '';
 
-	// const $search = (
-	// 	<button className = 'valleForm__input__button'>
-	// 		<svg className = 'valleForm__input__icon' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z"/></svg>
-	// 	</button>
-	// )
+	var $search = _react2['default'].createElement(
+		'button',
+		{ className: 'valleForm__input__button' },
+		_react2['default'].createElement(
+			'svg',
+			{ className: 'valleForm__input__icon', xmlns: 'http://www.w3.org/2000/svg', width: '24', height: '24', viewBox: '0 0 24 24' },
+			_react2['default'].createElement('path', { d: 'M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z' })
+		)
+	);
 
-	return (
-		// <span className = 'valleForm__input__container'>
+	var isFindAction = void 0;
 
+	if (Array.isArray(field.actions)) {
+
+		field.actions.forEach(function (current) {
+			if (current.action == 'find') isFindAction = true;
+		});
+	}
+
+	return _react2['default'].createElement(
+		'span',
+		{ className: 'valleForm__input__container' },
 		_react2['default'].createElement('valle-input', _extends({
 			value: field.value ? field.value : null,
 			'class': 'valleForm__input ' + customDescriptionStyle,
@@ -65,11 +78,7 @@ exports['default'] = function (field) {
 			id: field.id
 			// onBlur = { _fetchData }
 			, pattern: field.pattern
-		}, (0, _normalizeRequired2['default'])(field.required), (0, _normalizeReadOnly2['default'])(readOnly ? true : field.readonly), (0, _normalizeCaseProp2['default'])(field['case']), (0, _normalizeMask2['default'])(field.mask)))
-
-		// {field.actions[1].action === 'find' ? $search : null}
-
-		// </span>
-
+		}, (0, _normalizeRequired2['default'])(field.required), (0, _normalizeReadOnly2['default'])(readOnly ? true : field.readonly), (0, _normalizeCaseProp2['default'])(field['case']), (0, _normalizeMask2['default'])(field.mask))),
+		isFindAction ? $search : null
 	);
 };

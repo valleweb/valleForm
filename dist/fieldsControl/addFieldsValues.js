c6427161481b624ce6b1f8702cc1b077cf3f880f
afonsopacifer <afonsopolis@gmail.com>
2021-02-19 10:28:55 -0300

    Fix fill fields with zero value

--- dist/fieldsControl/addFieldsValues.js
+++ dist/fieldsControl/addFieldsValues.js
@@ -12,10 +12,19 @@ exports["default"] = function (defaultFieldsValues, _id) {
   var formScope = document.getElementById(_id);
   var allFields = formScope.querySelectorAll("[data-valle-field]");
 
+  // console.log(defaultFieldsValues);
+
   allFields.forEach(function (field) {
+
     var fieldKey = field.dataset.valleField;
 
-    if (defaultFieldsValues[fieldKey]) {
+    if (defaultFieldsValues[fieldKey] || defaultFieldsValues[fieldKey] == 0) {
+
+      // console.log('---------------')
+      // console.log(field.id)
+      // console.log(defaultFieldsValues[fieldKey])
+      // console.log('---------------')
+
       field.value = defaultFieldsValues[fieldKey];
     }
   });
--- src/fieldsControl/addFieldsValues.js
+++ src/fieldsControl/addFieldsValues.js
@@ -8,10 +8,19 @@ export default (defaultFieldsValues, _id) => {
   const formScope =  document.getElementById(_id);
   const allFields = formScope.querySelectorAll(`[data-valle-field]`);
 
+  // console.log(defaultFieldsValues);
+
   allFields.forEach(field => {
+
     const fieldKey = field.dataset.valleField;
 
-    if(defaultFieldsValues[fieldKey]) {
+    if(defaultFieldsValues[fieldKey] || defaultFieldsValues[fieldKey] == 0) {
+
+      // console.log('---------------')
+      // console.log(field.id)
+      // console.log(defaultFieldsValues[fieldKey])
+      // console.log('---------------')
+
       field.value = defaultFieldsValues[fieldKey];
     }
 
